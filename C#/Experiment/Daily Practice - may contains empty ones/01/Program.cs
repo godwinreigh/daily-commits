18d8af7ba4a9830286260551638d65b26b531056
SuuRo <godwinnariogalvez@gmail.com>
2020-12-29 13:49:43 +0800

    Changes to be committed:

---
+++ C#/Experiment/Daily Practice - may contains empty ones/01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //do loop
+            //do loops checks for the conditon first before looping
+            int isTrue = 1;
+            do
+            {
+                Console.WriteLine($"Itteration {isTrue}");
+                isTrue++;
+            } while (isTrue != 6);
+            //A program that checks if the number is divisible by 3 in range
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine($"{i} is Divisible by 3");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} is not Divisible by 3");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Experiment/Daily Practice - may contains empty ones/02/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //use var when handling List cause it handles both int and string
+            var names = new List<string> { "name", "Ana", "Felipe" };
+            names.Add("Maria");
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+            Console.WriteLine(names.Count);
+
+            var index = names.IndexOf("Felipe");//returns -1 if the item is not found
+            Console.WriteLine(index);
+
+            if (index != -1)
+            {
+                Console.WriteLine($"The name {names[index]} is at index {index}");
+            }
+            else
+            {
+                var notFound = names.IndexOf("Not Found");
+                Console.WriteLine($"When an item is not found, IndexOf return {notFound}");
+            }
+
+            //Sort
+            names.Sort();
+            Console.WriteLine("-------------------------");
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
